df8f3012f0553c846ca534a86b713ed90b4c32ff
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-05-25 23:35:23 +0530

    n = int(input()) count=0 x=0 y=1 while(count<n):     print(x,end=' ')     d = x + y     x = y     y = d     count+=1

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,10 @@
 n = int(input())
-a = 0
-b = 1
-c = a+b
-cnt = 0
-while(cnt<n):
-    print(a,end=' ')
-    a = b
-    b = c
-    c = a+b
-    cnt+=1
+count=0
+x=0
+y=1
+while(count<n):
+    print(x,end=' ')
+    d = x + y
+    x = y
+    y = d
+    count+=1
