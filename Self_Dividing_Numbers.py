dac37d1ecd400793e6b8504dec9409c74a538e2a
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-06 00:03:17 +0530

    def fun(n):     l = len(str(n))     x = n     count=0     while(n!=0):         t = n%10         if t==0:             count=count         elif (x%t==0):             count+=1         n=n//10     if count==l:         return 1     else:         return 0 m = int(input()) n = int(input()) for i in range(m,n+1):     if fun(i)==1:         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,20 +1,20 @@
-def selfd(n):
-    l=len(str(n))
-    cnt=0
-    r=n
-    while n!=0:
-        t=n%10
-        if(t==0):
-            cnt=cnt
-        elif r%t==0:
-            cnt+=1
+def fun(n):
+    l = len(str(n))
+    x = n 
+    count=0
+    while(n!=0):
+        t = n%10
+        if t==0:
+            count=count
+        elif (x%t==0):
+            count+=1
         n=n//10
-    if cnt==l:
+    if count==l:
         return 1
     else:
         return 0
 m = int(input())
 n = int(input())
 for i in range(m,n+1):
-    if selfd(i)==1:
+    if fun(i)==1:
         print(i,end=" ")
