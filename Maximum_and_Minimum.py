8ae02c5075d8857002cca60ceafa7889c67d128a
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-06 17:02:45 +0530

    n = int(input()) k = list(map(int,input().split())) m = [] c = 0 for i in k:     if i==k.count(i):         if i not in m:             m.append(i)             c = 1
    if c==1:
        print(min(m),max(m))
    else:
        print("-1")

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,14 @@
+n = int(input())
+k = list(map(int,input().split()))
+m = []
+c = 0
+for i in k:
+    if i==k.count(i):
+        if i not in m:
+            m.append(i)
+            c = 1
+            
+if c==1:
+    print(min(m),max(m))
+else:
+    print("-1")
