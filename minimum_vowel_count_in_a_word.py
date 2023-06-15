57656fe38d2ec88cdc3db1dee3ebcf8b831b84e6
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-15 05:47:10 +0530

    n = input() n = n.split() t = [] for i in range(len(n)):     c = 0     r = n[i]     k = "aeiouAEIOU"     for i in range(len(r)):         if r[i] in k:             c+=1     t.append(c) print(min(t))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,12 @@
+n = input()
+n = n.split()
+t = []
+for i in range(len(n)):
+    c = 0
+    r = n[i]
+    k = "aeiouAEIOU"
+    for i in range(len(r)):
+        if r[i] in k:
+            c+=1
+    t.append(c)
+print(min(t))
