55498b3e8b937391ab5515648c9d09281c91b656
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-16 03:03:14 +0530

    n = int(input()) l = list(map(int,input().split())) b = [] for i in range(0,n,2):     s = l[i]     s1 = l[i+1]     for i in range(1,s1+1):         b.append(s) print(*b)

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -2,8 +2,8 @@ n = int(input())
 l = list(map(int,input().split()))
 b = []
 for i in range(0,n,2):
-    k = l[i]
-    k1 = l[i+1]
-    for i in range(1,k1+1):
-        b.append(k)
+    s = l[i]
+    s1 = l[i+1]
+    for i in range(1,s1+1):
+        b.append(s)
 print(*b)
